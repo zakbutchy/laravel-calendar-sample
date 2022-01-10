8ca17e8ac93069e39a4b8855940b9d3685362a06
zakbutchy <zak.butchy@gmail.com>
2022-01-10 15:47:26 +0900

    add calendar and event controller

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Calendar;
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    public function index()
+    {
+        Calendar::all();
+    }
+
+    public function show(int $id)
+    {
+        return response()->json(Calendar::find($id));
+    }
+
+    // 新規
+    public function create(Request $request)
+    {
+        $calendar = new Calendar();
+
+        return $this->_saveCalendar($request, $calendar);
+    }
+
+    // 更新
+    public function save(Request $request)
+    {
+        $calendar = Calendar::find($request->id);
+
+        return $this->_saveCalendar($request, $calendar);
+    }
+
+    // 削除
+    public function destroy(Request $request)
+    {
+        $calendar = Calendar::find($request->id);
+
+        if ($calendar->delete()) {
+            return response()->json($calendar);
+        } else {
+            return response()->json(['error', 'Delete Error']);
+        }
+    }
+
+    // データ保存処理
+    private function _saveCalendar(Request $request, $calendar)
+    {
+        $calendar->name = $request->input('name');
+        $calendar->color = $request->input('color');
+        $calendar->visibility = $request->input('visibility');
+        $calendar->user_id = $request->input('user_id');
+
+        if ($calendar->save()) {
+            return response()->json($calendar);
+        } else {
+            return response()->json(['error' => 'Save Error']);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\CalendarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// カレンダー
+Route::group(['prefix' => 'calendars', 'as' => 'calendars.'], function () {
+    Route::get('/', [CalendarController::class, 'index'])->name('index');
+    Route::get("/{id}", [CalendarController::class, 'show'])->name('show');
+    Route::post('/', [CalendarController::class, 'create'])->name('create');
+    Route::put('/{id}', [CalendarController::class, 'save'])->name('update');
+    Route::delete('/{id}', [CalendarController::class, 'destroy'])->name('delete');
+});
+
+// イベント
+Route::group(['prefix' => 'events', 'as' => 'events.'], function () {
+    Route::get('/', [EventController::class, 'index'])->name('index');
+    Route::get("/{id}", [EventController::class, 'show'])->name('show');
+    Route::post('/', [EventController::class, 'create'])->name('create');
+    Route::put('/{id}', [EventController::class, 'save'])->name('update');
+    Route::delete('/{id}', [EventController::class, 'destroy'])->name('delete');
 });
